bfe690865071a2dffa1f33af83b004e0bfcd0f0c
Divkov575 <dmitriy@ivkov.net>
2024-05-16 19:31:52 -0400

    fixed bug -> truncate existing config file when updating

--- src/remove.rs
+++ src/remove.rs
@@ -2,7 +2,7 @@ use std::fs::{File, read_to_string};
 use std::process::exit;
 use anyhow::Result;
 use dirs::home_dir;
-use crate::{check_for_config_existence, Config, CsError, gen_id, get_ids, Record};
+use crate::{check_for_config_existence, Config, CsError, gen_id, get_ids, Record, show};
 
 pub fn remove(id: String) -> Result<()> {
     let home_dir = home_dir().ok_or(CsError::MissingHomeDir)?; // attempt to get home env-var
@@ -11,13 +11,16 @@ pub fn remove(id: String) -> Result<()> {
 
     let mut config: Config = serde_yaml::from_str(&read_to_string(&config_path)?)?;
     let ids = get_ids(&config)?;
-
+    let id = id.to_uppercase();
 
     let index = ids.iter().position(|x| x == &id).ok_or(CsError::NonExistentId(id.clone()))?;
     config.data.remove(index);
 
-    let file = File::options().write(true).open(&config_path)?;
+    let file = File::options().write(true).truncate(true).open(&config_path)?;
     serde_yaml::to_writer(file, &config)?;
+
+    println!("Success removing record");
+    show::show()?;
 
     Ok(())
 }
